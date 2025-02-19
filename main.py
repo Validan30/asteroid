06a8af970a09637a5a2e7dea5774bc5c761da55e
Validan30 <validus17@gmail.com>
2025-02-19 14:37:47 -0800

    "D: Added clock function"

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 from constants import *
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    framerate = 60
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,6 +15,8 @@ def main():
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = ((clock.tick(60))/1000)
 
 if __name__ == "__main__":
     main()
